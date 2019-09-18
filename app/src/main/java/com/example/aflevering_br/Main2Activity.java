4893ed136652ee38c8a2a1cbc7531bc805d7f2d3
Sbicen99 <sercanbicen99@gmail.com>
2019-09-18 15:18:12 +0200

    Første Commit

---
+++ app/src/main/java/com/example/aflevering_br/Main2Activity.java
@@ -0,0 +1,24 @@
+package com.example.aflevering_br;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Main2Activity extends AppCompatActivity {
+
+private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        webView = findViewById(R.id.wb);
+        webView.loadUrl("https://javabog.dk/");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+
+    }
+}
---
+++ app/src/main/java/com/example/aflevering_br/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.aflevering_br;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button minKnap, minKnap2;
+    TextView textView3;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+       minKnap2 = findViewById(R.id.minKnap2);
+       minKnap = findViewById(R.id.minKnap);
+
+
+        minKnap2.setOnClickListener(this);
+        minKnap.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+            textView3.setText("Tak for at du trykkede! - tryk også på OK, for at se noget magisk igen :-)");
+
+
+            }
+        });
+
+            textView3 = findViewById(R.id.textView3);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+
+
+        System.out.println("Hejhej");
+
+
+    }
+}
+
